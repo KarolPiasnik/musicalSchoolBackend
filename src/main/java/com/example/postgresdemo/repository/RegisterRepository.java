2e2a27461460f569bf267987c7f08ab087ce9ed3
Karol Piasnik <piasnik.karol@outlook.com>
2018-11-23 22:41:05 +0100

    add queries

--- src/main/java/com/example/postgresdemo/repository/RegisterRepository.java
+++ src/main/java/com/example/postgresdemo/repository/RegisterRepository.java
@@ -2,9 +2,16 @@ package com.example.postgresdemo.repository;
 
 import com.example.postgresdemo.model.Question;
 import com.example.postgresdemo.model.Register;
+import com.example.postgresdemo.model.Student;
+import com.example.postgresdemo.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RegisterRepository extends JpaRepository<Register, Long> {
+    List<Register> findBySubjectId(Long subjectId);
+    List<Teacher> findByTeacherId(Long teacherId);
+
 }
