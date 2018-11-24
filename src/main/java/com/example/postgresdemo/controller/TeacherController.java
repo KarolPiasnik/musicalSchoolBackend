9d279aa79fd70a5e41bc91add5ff7b9e67672693
Karol Piasnik <piasnik.karol@outlook.com>
2018-11-24 16:35:28 +0100

    fix adding teachers

--- src/main/java/com/example/postgresdemo/controller/TeacherController.java
+++ src/main/java/com/example/postgresdemo/controller/TeacherController.java
@@ -6,6 +6,7 @@ import com.example.postgresdemo.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,15 +19,21 @@ public class TeacherController {
     private TeacherRepository teacherRepository;
 
     @GetMapping("/teachers")
-    public Page<Teacher> getTeachers(Pageable pageable) {
+    public Page<Teacher> getTeachers(@PageableDefault(value=50) Pageable pageable) {
         return teacherRepository.findAll(pageable);
     }
 
 
     @PostMapping("/teachers")
-    public Teacher createTeacher(@Valid @RequestBody Teacher teacher) {
+    public Teacher createTeacher( @Valid @RequestBody Teacher teacher) {
+        System.out.println(teacher.getName());
+        System.out.println(teacher.getAddress());
+        System.out.println(teacher.getSurname());
+        System.out.println(teacher.getPesel());
+       // System.out.println(teacher.getActive());
         return teacherRepository.save(teacher);
     }
+
 
     @PutMapping("/teachers/{teacherId}")
     public Teacher updateTeacher(@PathVariable Long teacherId,
--- src/main/java/com/example/postgresdemo/model/Teacher.java
+++ src/main/java/com/example/postgresdemo/model/Teacher.java
@@ -1,11 +1,12 @@
 package com.example.postgresdemo.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Entity
 @Table(name = "teachers")
-public class Teacher extends Person {
+public class Teacher extends AuditModel {
     @Id
     @GeneratedValue(generator = "teacher_generator")
     @SequenceGenerator(
@@ -14,6 +15,77 @@ public class Teacher extends Person {
             initialValue = 1000
     )
     private Long id;
+
+    @NotBlank
+    private String pesel;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String surname;
+
+    @NotBlank
+    private String address;
+
+//    public Boolean getActive() {
+//        return active;
+//    }
+//
+//    public void setActive(Boolean active) {
+//        active = active;
+//    }
+
+//    @NotNull
+//    private Boolean active;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Register> getRegisters() {
+        return registers;
+    }
+
+    public void setRegisters(List<Register> registers) {
+        this.registers = registers;
+    }
 
     @OneToMany(mappedBy = "teacher")
     private List<Register> registers;
