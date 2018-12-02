7101fac6915080efba1a6e7cf096c1be498134e1
Karol Piasnik <piasnik.karol@outlook.com>
2018-12-02 17:34:15 +0100

    add student to gradebook endpoint

--- src/main/java/com/example/postgresdemo/controller/GradebookController.java
+++ src/main/java/com/example/postgresdemo/controller/GradebookController.java
@@ -2,22 +2,30 @@ package com.example.postgresdemo.controller;
 
 import com.example.postgresdemo.exception.ResourceNotFoundException;
 import com.example.postgresdemo.model.Gradebook;
+import com.example.postgresdemo.model.Student;
 import com.example.postgresdemo.model.Subject;
 import com.example.postgresdemo.model.Teacher;
 import com.example.postgresdemo.repository.GradebookRepository;
+import com.example.postgresdemo.repository.StudentRepository;
 import com.example.postgresdemo.repository.SubjectRepository;
 import com.example.postgresdemo.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
 public class GradebookController {
+
+    @Autowired
+    StudentRepository studentRepository;
 
     @Autowired
     private GradebookRepository gradebookRepository;
@@ -39,6 +47,37 @@ public class GradebookController {
         return gradebookRepository.findById(gradebookId).get();
     }
 
+    @PostMapping("/api/gradebooks/{gradebookId}/students")
+    public Gradebook addStudentToGradebook(@PathVariable Long gradebookId, @Valid @RequestBody Student student) {
+        Gradebook gradebook = gradebookRepository.findById(gradebookId).get();
+        if (gradebook != null) {
+            if (student != null) {
+                if (student.getId() != null) {
+                    Optional<Student> studentToAdd = studentRepository.findById(student.getId());
+                    if (studentToAdd.isPresent()) {
+                        List<Student> students = new LinkedList<>();
+                        students = gradebook.getStudents();
+                        if(students.contains(studentToAdd.get())){
+                            throw new DuplicateKeyException("Nie można dodać 2 razy tego samego studenta");
+                        }
+                        students.add(studentToAdd.get());
+
+                        gradebook.setStudents(students);
+                        return gradebookRepository.save(gradebook);
+                    }
+                    else {
+                        throw new NoSuchElementException("No student with such ID in database");
+                    }
+                } else {
+                    throw new NoSuchElementException("Provided student has no ID");
+                }
+            } else {
+                throw new NoSuchElementException("No student in request");
+            }
+        } else {
+            throw new NoSuchElementException("gradebook");
+        }
+    }
 
     @PostMapping("/api/gradebooks")
     public Gradebook createGradebook(@Valid @RequestBody Gradebook gradebook) {
